fe94ec81495c7b9e84e4c052a74352622705ccc8
SolomonUg <solomonchris700@gmail.com>
2025-05-28 15:16:42 +0100

    remove reset funcition as its not used

--- app/error.tsx
+++ app/error.tsx
@@ -3,10 +3,9 @@ import Link from "next/link";
 
 interface ErrorProps {
   error: Error;
-  reset: () => void;
 }
 
-export default function Error({ error, reset }: ErrorProps) {
+export default function Error({ error }: ErrorProps) {
   return (
     <main className="flex justify-center items-center flex-col gap-6">
       <h1 className="text-3xl font-semibold">Something went wrong!</h1>
